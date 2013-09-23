bb0c59ad7ced0ed99cdf00aaab18c4e87e43b2c3
Phantium <git@phantium.com>
2013-09-23 15:33:31 +0200

    Update folder structure

---
+++ webapp/read_webapp-settings.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from MAPI import *
+from MAPI.Util import *
+import sys
+
+def check_input():
+        if len(sys.argv) < 2:
+            sys.exit('Usage: %s username' % sys.argv[0])
+
+def read_settings():
+        settings = None 
+        data = None
+
+        PR_EC_WEBACCESS_SETTINGS_JSON = PROP_TAG(PT_STRING8, PR_EC_BASE+0x72)
+
+        s = OpenECSession(sys.argv[1], '', 'file:///var/run/zarafa')
+        st = GetDefaultStore(s)
+
+        try:
+                settings = st.OpenProperty(PR_EC_WEBACCESS_SETTINGS_JSON, IID_IStream, 0, 0)
+                data = settings.Read(4096)
+        except:
+                print 'User has not used WebApp yet, no settings property exists.'
+
+        if not data:
+                data = 'No settings present.'
+        elif data == '{"settings":{"zarafa":{"v1":{"contexts":{"mail":[]}}}}}':
+                print 'Default settings present, user has not yet logged in.'
+        else:
+                print data
+
+if __name__ == '__main__':
+        check_input()
+        read_settings()
