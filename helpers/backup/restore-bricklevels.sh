ae0f5747b5512bab9f584e13358220e491955914
Phantium <git@phantium.com>
2014-02-21 19:18:22 +0100

    Add restore-bricklevels.sh Allows you to recover all bricklevel backups in a directory by going through the zarafa-admin -l userlist. And checks if the file exists for each individual user.

---
+++ helpers/backup/restore-bricklevels.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+if [ -e /usr/share/zarafa-backup/full-restore.sh ];
+then
+   RESTORE=/usr/share/zarafa-backup/full-restore.sh
+elif [ -e /usr/share/zarafa/full-restore.sh ];
+then
+   RESTORE=/usr/share/zarafa/full-restore.sh
+else
+   echo Error: Cannot find full-restore.sh.
+   exit 1
+fi
+
+for user in `zarafa-admin -l | egrep -v "\-|SYSTEM|User list|Username" | awk '{print $1}'`;
+do
+   echo ------
+   echo Starting bricklevel import for user: $user
+   if [ -e $user.index.zbk ];
+   then
+       echo Importing bricklevel for user: $user
+       echo $RESTORE $user
+   else
+       echo Cannot find bricklevel for user: $user, skipping.
+   fi
+done
