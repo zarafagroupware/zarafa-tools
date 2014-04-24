502dec31e35ddf0a1a56669fbbbc729550c1632a
Phantium <git@phantium.com>
2014-04-24 13:38:44 +0200

    Fixed a bug where the script would show users with the correct version.

--- helpers/stats/clientversions.sh
+++ helpers/stats/clientversions.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-ZCPVERSION=$(zarafa-admin -V|grep "Product"|awk '{print $NF}'|tr ',' '.'|sed 's/\(.*\)\./\1-/')
+ZCPVERSION=$(zarafa-admin -V|grep "Product"|awk '{print $NF}'|tr ',' '.')
 STATS=$(zarafa-stats --session | awk '{print $6, $11, $3, $12}'| egrep -v "0x6746000B|SYSTEM"|grep "\,"|grep -i "OUTLOOK"|tr ' ' ';'|sort|uniq|grep -v $ZCPVERSION)
 
-echo -e "Running ZCP version: $ZCPVERSION\n---"
+echo -e "Running ZCP version: $(echo $ZCPVERSION|sed 's/\(.*\)\./\1-/')\n---"
 echo -e "Clients using an old client version:\n"
 for user in $STATS
 do
-        username=$(echo $user|cut -d';' -f1)
-        version=$(echo $user|cut -d';' -f2|tr ',' '.'|sed 's/\(.*\)\./\1-/')
-        address=$(echo $user|cut -d';' -f3)
-        echo "User: $username is using version: $version from IP: $address"
+    username=$(echo $user|cut -d';' -f1)
+    version=$(echo $user|cut -d';' -f2|tr ',' '.'|sed 's/\(.*\)\./\1-/')
+    address=$(echo $user|cut -d';' -f3)
+    echo "User: $username is using version: $version from IP: $address"
 done
