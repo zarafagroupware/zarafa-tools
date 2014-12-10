c6abd6ac48c95e67d66912ad8d67688f5ac2cd63
Felix Bartels <felix@host-consultants.de>
2014-12-10 15:01:33 +0100

    add hint in code about users on remote servers

--- helpers/backup/zarafa-backup-archives.py
+++ helpers/backup/zarafa-backup-archives.py
@@ -6,7 +6,8 @@ z = zarafa.Server()
 
 def backup_archives():
 
-	for user in z.users():
+	#for user in z.users(remote=True): # Backup users that are on external servers as well
+        for user in z.users(): # Backup only local users
 		if user.archive_store:
 			if not os.path.isdir(user.name):
 				os.mkdir(user.name)
