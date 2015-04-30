1b09dbb7648c2088eba70b2dbb149c1523417edf
Felix Bartels <felix@host-consultants.de>
2015-04-30 07:26:38 +0200

    make script run in cron, replace spaces with tabs, add check to abort in empty $LASTDATE to avoid date errors further in the script

--- helpers/z-push/z-push-delete-old-states.sh
+++ helpers/z-push/z-push-delete-old-states.sh
@@ -2,22 +2,31 @@
 IFS=$'\n'
 ZPA=/usr/share/z-push/z-push-admin.php
 
+# set terminal in case the script is called from cron
+export TERM="xterm"
+
 TWOMONTHSAGO=$(date +"%s" --date='2 months ago')
 
 for i in $($ZPA -a lastsync | tail -n +6); do
-        DEVICE=$(echo $i | awk '{print $1}')
-        USER=$(echo $i | awk '{print $2}')
-        LASTDATE=$(echo $i | awk '{print $3}')
+	DEVICE=$(echo $i | awk '{print $1}')
+	USER=$(echo $i | awk '{print $2}')
+	LASTDATE=$(echo $i | awk '{print $3}')
 
-        if [[ $LASTDATE == *"never"* ]]; then
-                echo "$DEVICE never synced"
-                $ZPA -a remove -u $USER -d $DEVICE
-                continue
-        else
-                LASTDATE2=$(date -d $LASTDATE +%s)
-        fi
-        if [[ $TWOMONTHSAGO -ge $LASTDATE2 ]]; then
-                echo "$DEVICE is older than 2 months"
-                $ZPA -a remove -u $USER -d $DEVICE
-        fi
+	if [[ -z $LASTDATE ]]; then
+		echo "something must have gone wrong"
+		exit 1
+	fi
+
+	if [[ $LASTDATE == *"never"* ]]; then
+		echo "$DEVICE never synced"
+		$ZPA -a remove -u $USER -d $DEVICE
+		continue
+	else
+		LASTDATE2=$(date -d $LASTDATE +%s)
+	fi
+
+	if [[ $TWOMONTHSAGO -ge $LASTDATE2 ]]; then
+		echo "$DEVICE is older than 2 months"
+		$ZPA -a remove -u $USER -d $DEVICE
+	fi
 done
