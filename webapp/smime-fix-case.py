4f3cb31afb45ec5cf1e27b1e203e4a3de1a74419
markb <m.debruijn@zarafa.com>
2015-08-12 11:10:54 +0200

    smime-fix-case.py, add additional check.

--- webapp/smime-fix-case.py
+++ webapp/smime-fix-case.py
@@ -9,11 +9,11 @@ z = zarafa.Server()
 def main():
     for user in z.users():
         for item in user.store.root.associated.items():
-            if item.prop(PR_MESSAGE_CLASS).value == 'Webapp.Security.Public':
-                print "Correcting item"
-                item.mapiobj.SetProps([SPropValue(PR_MESSAGE_CLASS, 'WebApp.Security.Public')])
-                item.mapiobj.SaveChanges(0)
-
+            if item.prop(PR_MESSAGE_CLASS):
+                if item.prop(PR_MESSAGE_CLASS).value == 'Webapp.Security.Public':
+                    print "Correcting item"
+                    item.mapiobj.SetProps([SPropValue(PR_MESSAGE_CLASS, 'WebApp.Security.Public')])
+                    item.mapiobj.SaveChanges(0)
 
 if __name__ == '__main__':
     main()
