f30fd8d9bb2b4d778fd0ef57755464ec6f94885a
Mark de Bruijn <m.debruijn@zarafa.com>
2015-12-07 14:27:45 +0100

    Skip the first 4 lines of the output of zarafa-admin -l as they contain no users at all.

--- mailstore/cleanupscripts/cleanup.sh
+++ mailstore/cleanupscripts/cleanup.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-
 echo 'Cleanup spam & deleted items folders'
 
-for name in $(zarafa-admin -l | awk '{print $1}')
+for name in $(zarafa-admin -l | tail -n +5 | awk '{print $1}')
 do	
+  echo "Processing for user $name"
   php /usr/local/bin/spam.php $name 1> /dev/null
   php /usr/local/bin/delete.php $name 1> /dev/null
   python /usr/local/bin/rssfeeds.py $name 1> /dev/null
 done
-
-exit 0
-
